8a129c928df1a009c4ef7f2e8a95a0b62314e2e1
yowhatsgoodhotmailid0987-a11y <yo.whatsgoodhotmail.id0987@gmail.com>
2026-03-12 23:06:26 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
-GAME_SERVER_IP = os.environ.get("GAME_SERVER_IP", "127.0.0.1")
+GAME_SERVER_IP = os.environ.get("GAME_SERVER_IP", "66.33.22.2")
 ROUTE_PREFIX = os.environ.get("ROUTE_PREFIX", "")
-HTTP_PORT = os.environ.get("HTTP_PORT", "9013")
+HTTP_PORT = os.environ.get("HTTP_PORT", "8000")
 DATABASE_PATH = os.environ.get("DATABASE_PATH", "zewmsm.db")
 HOME = os.environ.get("HOME", ".")
